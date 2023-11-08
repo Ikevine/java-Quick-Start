ee429c50cd03bd8e6563e49546266971dfabdfa1
Ikevine <ishimwekevine098@gmail.com>
2023-11-08 06:37:17 +0200

    myFirst unit testing

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	//crating test underTesting method
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void addTwoNumbers(){
+		//given
+		int num1 = 10;
+		int num2 = 20;
+		//when
+		int result = underTest.add(num1, num2);
+		//then
+		Assert.assertEquals(30 , result);
+	}
+	class Calculator{
+		int add (int a ,int b){
+			return a+b;
+		}
 	}
 
 }
